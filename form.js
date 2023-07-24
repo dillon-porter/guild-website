47ea075e29a2351d61e8af6459f9cf0ee529649f
Dillon Porter <dillonporter@hotmail.com>
2023-07-24 00:10:37 -0230

    update guild website for light mode theme

---
+++ form.js
@@ -0,0 +1,76 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const classSelect = document.getElementById('classSelect');
+    const warlockSpecs = document.getElementById('warlockSpecs');
+    const mageSpecs = document.getElementById('mageSpecs');
+    const rogueSpecs = document.getElementById('rogueSpecs');
+    const demonhunterSpecs = document.getElementById('demonhunterSpecs');
+    const evokerSpecs = document.getElementById('evokerSpecs');
+    const monkSpecs = document.getElementById('monkSpecs');
+    const hunterSpecs = document.getElementById('hunterSpecs');
+    const shamanSpecs = document.getElementById('shamanSpecs');
+
+    classSelect.addEventListener('change', function() {
+        if (classSelect.value === 'warlock') {
+            warlockSpecs.style.display = 'block';
+        } else {
+            warlockSpecs.style.display = 'none';
+        }
+    });
+
+    classSelect.addEventListener('change', function() {
+        if (classSelect.value === 'mage') {
+            mageSpecs.style.display = 'block';
+        } else {
+            mageSpecs.style.display = 'none';
+        }
+    });
+
+    classSelect.addEventListener('change', function() {
+        if (classSelect.value === 'rogue') {
+            rogueSpecs.style.display = 'block';
+        } else {
+            rogueSpecs.style.display = 'none';
+        }
+    });
+
+    classSelect.addEventListener('change', function() {
+        if (classSelect.value === 'demon-hunter') {
+            demonhunterSpecs.style.display = 'block';
+        } else {
+            demonhunterSpecs.style.display = 'none';
+        }
+    });
+
+    classSelect.addEventListener('change', function() {
+        if (classSelect.value === 'evoker') {
+            evokerSpecs.style.display = 'block';
+        } else {
+            evokerSpecs.style.display = 'none';
+        }
+    });
+
+    classSelect.addEventListener('change', function() {
+        if (classSelect.value === 'monk') {
+            monkSpecs.style.display = 'block';
+        } else {
+            monkSpecs.style.display = 'none';
+        }
+    });
+
+    classSelect.addEventListener('change', function() {
+        if (classSelect.value === 'hunter') {
+            hunterSpecs.style.display = 'block';
+        } else {
+            hunterSpecs.style.display = 'none';
+        }
+    });
+
+    classSelect.addEventListener('change', function() {
+        if (classSelect.value === 'shaman') {
+            shamanSpecs.style.display = 'block';
+        } else {
+            shamanSpecs.style.display = 'none';
+        }
+    });
+
+});
--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-const menu = document.getElementById("mobile-menu")
-const menuLinks = document.querySelector(".nav-menu")
+const menu = document.getElementById("mobile-menu");
+const menuLinks = document.querySelector(".nav-menu");
 
 menu.addEventListener("click", function() {
-    menu.classList.toggle('is-active')
-    menuLinks.classList.toggle('active')
-})
+    menu.classList.toggle("is-active");
+    menuLinks.classList.toggle("active");
+});
+
+// Smooth scrolling for navbar links and buttons
+const navLinks = document.querySelectorAll(".nav-links, .nav-links-btn");
+
+navLinks.forEach(function(navLink) {
+    navLink.addEventListener("click", function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute("href");
+        const targetElement = document.querySelector(targetId);
+        targetElement.scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
+
+function toggleTheme() {
+    const body = document.body;
+    const isDarkMode = body.classList.contains("dark-mode");
+
+    if (isDarkMode) {
+        body.classList.remove("dark-mode");
+        localStorage.setItem("theme", "light");
+    } else {
+        body.classList.add("dark-mode");
+        localStorage.setItem("theme", "dark");
+    }
+}
+
+// Check the theme preference from localStorage on page load
+window.addEventListener("DOMContentLoaded", () => {
+    const theme = localStorage.getItem("theme");
+    if (theme === "dark") {
+        document.body.classList.add("dark-mode");
+    }
+});
