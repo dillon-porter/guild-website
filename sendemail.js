1fe1a2c91e17362dd9e77695effbab765c924ef2
Dillon Porter <dillonporter@hotmail.com>
2023-07-25 18:41:35 -0230

    update form

--- sendemail.js
+++ sendemail.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 exports.handler = async function(event, context) {
     try {
         const transporter = nodemailer.createTransport({
-            service: 'gmail',
+            service: 'outlook',
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASSWORD,
